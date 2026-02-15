495ae4ad478bf606fc13f269e3b0fed61e066732
VincentDoherty <vpjd800@gmail.com>
2026-02-15 23:40:37 +0000

    B: FPS

--- main.py
+++ main.py
@@ -10,14 +10,23 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0 
 
     while True:
         log_state()
+        
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    screen.fill("black")  # Fill screen with black color
-    display.flip()  # Update the display
+            
+            
+        screen.fill("black")  # Fill screen with black color
+        pygame.display.flip()  # Update the display
+        
+        
+        dt = clock.tick(60) / 1000  
 
 if __name__ == "__main__":
     main()
